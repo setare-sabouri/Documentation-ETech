c6f3732e6273b83a5bac08e2025d811fcac82c40
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-26 00:08:58 +0200

    find the product by slug

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>E-Tech Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/setare-sabouri/ecommerce',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/setare-sabouri/ecommerce',
   footer: {
     text: 'Nextra Docs Template',
   },
